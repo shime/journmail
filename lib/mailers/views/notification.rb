0c30509cf5e73025073b314962006a668f0d8469
Hrvoje Šimić <shime@twobucks.co>
2017-12-11 19:58:55 +0100

    feat: start giving URL to history page in notification emails

--- lib/mailers/views/notification.rb
+++ lib/mailers/views/notification.rb
@@ -7,6 +7,7 @@ class NotificationView
   def initialize(user, company_name = "Vedran & Hrvoje")
     @user = user
     @unsubscribe_link = link_to_unsubscribe(user)
+    @history_link = link_to_history(user)
     @company_name = company_name
     @streak_body = StreakBody.call(user)
   end
@@ -21,5 +22,9 @@ class NotificationView
 
     def link_to_unsubscribe(user)
       "#{Settings.urls.unsubscribe}/#{user.token}"
+    end
+
+    def link_to_history(user)
+      "#{Settings.urls.history}/#{user.token}
     end
 end
